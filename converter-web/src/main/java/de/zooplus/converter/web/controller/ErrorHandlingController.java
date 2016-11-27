c56302e2f13cdb8c68b30ec69a46255329df061b
dragan susak <dragan.susak@live.com>
2016-11-27 16:27:47 +0100

    fix no currency found

--- converter-web/src/main/java/de/zooplus/converter/web/controller/ErrorHandlingController.java
+++ converter-web/src/main/java/de/zooplus/converter/web/controller/ErrorHandlingController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
@@ -14,6 +15,12 @@ public class ErrorHandlingController {
 
     @ExceptionHandler(value = Exception.class)
     public String onError(Exception ex) {
+        if(ex instanceof HttpClientErrorException){
+            HttpClientErrorException httpClientErrorException = (HttpClientErrorException)ex;
+            if(httpClientErrorException.getStatusCode() == HttpStatus.UNPROCESSABLE_ENTITY){
+                return "error/conversionNotFound";
+            }
+        }
         return "error/error";
     }
 
---
+++ converter-web/src/main/webapp/WEB-INF/views/error/conversionNotFound.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Couldn't retrieve data</title>
+</head>
+<body>
+Couldn't retrieve the data from external service.
+Please try different currencies or different date.
+Go to <a href="/converter/home">Home</a> page.
+</body>
+</html>
